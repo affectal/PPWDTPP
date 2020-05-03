232cde9ff13e02877cdeb4feb42e103e742dbffc
Affectal <aff.p@yandex.ru>
2020-05-03 23:30:28 +0700

    added header, bigSlider and features

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+
+    $('.bigSlider__inner').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+    });
+
+});
