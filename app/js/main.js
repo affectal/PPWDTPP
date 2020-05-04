20cd42fd470301cf7ff0ffd65f4cc7cae268059a
Affectal <aff.p@yandex.ru>
2020-05-04 17:16:48 +0700

    completed home page

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,18 @@
 $(function() {
 
     $('.bigSlider__inner').slick({
-        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
-        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        nextArrow: '<button type="button" class="bigSlick-btn bigSlick-next"></button>',
+        prevArrow: '<button type="button" class="bigSlick-btn bigSlick-prev"></button>',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+    });
+
+
+    $('.newsSlider__inner').slick({
+        nextArrow: '<button type="button" class="newsSlick-btn newsSlick-next"></button>',
+        prevArrow: '<button type="button" class="newsSlick-btn newsSlick-prev"></button>',
         slidesToShow: 1,
         slidesToScroll: 1,
         dots: false,
