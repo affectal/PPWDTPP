f4d65f31313f92b8c57447c932c65287a56c8340
Affectal <aff.p@yandex.ru>
2020-05-06 21:51:44 +0700

    renamed: productPage.html -> productsPage.html, added ProductInnerPage.html

--- app/js/main.js
+++ app/js/main.js
@@ -24,7 +24,7 @@ $(function() {
     });
 
 
-    $('.product__slider').slick({
+    $('.products__slider').slick({
         nextArrow: '<button type="button" class="prodSlick-btn prodSlick-next"></button>',
         prevArrow: '<button type="button" class="prodSlick-btn prodSlick-prev"></button>',
         slidesToShow: 1,
@@ -34,16 +34,16 @@ $(function() {
     });
 
     $('.tabPopular').on('click', function() {
-        $('.productPage__inner.popular').show();
-        $('.productPage__inner.recent').hide();
+        $('.productsPage__inner.popular').show();
+        $('.productsPage__inner.recent').hide();
         $('.tabPopular').css('background-color', '#434356');
         $('.tabRecent').css('background-color', '#ceced0');
     });
 
     $('.tabRecent').on('click', function() {
-        $('.productPage__inner.recent').show();
-        $('.productPage__inner.popular').hide();
-        $('.productPage__inner.recent').css('display', 'flex');
+        $('.productsPage__inner.recent').show();
+        $('.productsPage__inner.popular').hide();
+        $('.productsPage__inner.recent').css('display', 'flex');
         $('.tabRecent').css('background-color', '#434356');
         $('.tabPopular').css('background-color', '#ceced0');
 
